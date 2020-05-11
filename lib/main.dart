ebe434e917d534992e125cf7e2adbee9b6ed02fb
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2020-05-11 18:11:11 +0530

    Initialized with Home Screen UI
    Enabled Multidex

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:cucekified/pages/home.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,7 +30,40 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Theme.of(context).accentColor,
+            Theme.of(context).primaryColor,
+          ],
+        ),
+      ),
       alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          // Logo
+          SvgPicture.asset(
+            'images/logo.svg',
+            height: 160,
+          ),
+          // Title
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('CUCEKIFIED')
+              ],
+            ),
+          ),
+          // Teacher and Student Navigator
+        ],
+      ),
     );
   }
 }
